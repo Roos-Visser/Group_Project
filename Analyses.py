9a7a0cb695641cc75713de71deb4397f7f56af07
Vivian van Oosten <fif@amvi.nl>
2020-01-20 16:08:36 +0100

    Python rock classification

--- Analyses.py
+++ Analyses.py
@@ -28,12 +28,23 @@ def convert_csv(filepath):
                 else:
                     list_data.append([entry["ontology/activeYearsStartYear"],entry["ontology/genre_label"]])
 
-            classification of the genres
-        for genre in list_data:
-            if genre[1] in ("Hard rock", "blues rock"):
-                genre[1] = "Rock music"
-            elif genre[1] in ("Britpop", "Christian alternative rock", "college rock", "dream pop", "emo", "geek rock", "gothic rock", "grunge", "indie rock", "math rock", "noise pop", "nu gaze", "post-grunge","post-punk revival", "post-rock", "shoegazing"):
-                genre[1] = "Alternative rock"
+        # for genre in list_data:
+        #     if genre[1] in ("Arena rock", "Psychedelic rock", "Garage rock", "Garage punk", "Soft rock"):
+        #         genre[1] = "Rock music"
+        #     elif genre[1] in ("grunge","Glam metal","Glam rock"):
+        #         genre[1] = "Hard rock"
+        #     elif genre[1] in ("Power metal", "Gothic metal", "Post-metal", "Alternative metal", "Trash metal", "Progressive metal", "Death metal", "Metalcore"):
+        #         genre[1] = "Heavy metal music"
+        #     elif genre[1] in ("Britpop", "Christian alternative rock", "Christian rock", "College rock", "Dream pop", "Emo", "Gothic rock", "Grunge", "Math rock", "Noise pop", "Noise rock", "Nu metal", "Post-grunge","Post-punk revival", "Post-rock", "Shoegazing"):
+        #         genre[1] = "Alternative rock"
+        #     elif genre[1] in ("Progressive rock", "Art rock", "Experimental rock"):
+        #         genre[1] = "Alternative rock"
+        #     elif genre[1] in ("Indie rock", "power pop"):
+        #         genre[1] = "Pop rock"
+        #     elif genre[1] in ("Hardcore punk", "Post-punk"):
+        #         genre[1] = "Punk rock"
+        #     elif genre[1] in ("Rockabilly", ...):
+        #         genre[1] = "Rhythm and blues"
             elif genre[1] in ("Art pop", "Bubblegum pop", "Dance-pop", "Experimental pop", "New wave music", "Operatic pop", "Progressive pop", "Sophisti-pop", "Space age pop", "Sunshine pop", "Synthpop", "Teen pop", "K-pop", "Electropop", "Latin pop", "Arabic pop music", "Indian pop", "Mexican pop music", "Europop", "French pop music", "Power pop", "Traditional pop music", "Operatic pop", "Baroque pop", "Britpop", "Art pop", "Balkan_pop", "Dream pop", "J-pop"):
                 genre[1] = "Pop music"
             elif genre[1] in ("Alternative hip hop", "Christian hip hop", )
