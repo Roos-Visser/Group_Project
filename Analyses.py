aad0ed61ddf08aea55f038f02199e97f332d5e9a
Mandrach <daniel_amg@web.de>
2020-01-20 12:28:42 +0100

    Removed the redundant multiple adding of the header
    The csv file now looks fine and how it is supposed to look

--- Analyses.py
+++ Analyses.py
@@ -16,11 +16,13 @@ def convert_csv(filepath):
     # creating a new csv file. 'a' specifies that that append the entries to the same file
     # That way we can create one csv file with all the relevant data we need
     with open('names_data.csv', 'a', encoding='utf8') as file:
-        file.write(f'genres, year \n')
+        
         for entry in data:
             if "ontology/activeYearsStartYear" in entry and 'ontology/genre_label' in entry and "ontology/background" in entry and entry["ontology/background"] == 'solo_singer':
                 file.write(f'{entry["ontology/activeYearsStartYear"]},{entry["ontology/genre_label"]}\n')
 
-alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+with open('names_data.csv', 'w', encoding='utf8') as file:
+    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    file.write(f'year, genre \n')
 for letter in alphabet:
     convert_csv(f'./People/{letter}_people.json')
